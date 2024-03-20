d97a9e5a363a0cac7ec9b54cd6c1b29e703060a2
Kcarazas <kcarazas@usm.cl>
2024-03-19 23:29:25 -0300

    tareas de programacion

---
+++ ejercicios.py
@@ -0,0 +1,15 @@
+"""
+from math import sqrt
+a=int(input("ingrese cateto a: "))
+b=int(input("ingrese cateto b: "))
+c = sqrt((a**2)+(b**2))
+print(rou"nd(c,2))
+x = float(input())
+n = int(input())
+c = int(input())
+a = 1+x/100
+b = a**n
+c = c*b
+final = round(c)
+print(final)
+"""
